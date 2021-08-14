89a6104ff3906f6d87a31f0f9699558df2985d99
salman10118389 <salman.10118389@mahasiswa.unikom.ac.id>
2021-08-14 19:51:58 +0700

    Revert "Uas AKB 10118389 Muhammad Salman Al-Farisi"
    This reverts commit 588c035c

--- app/src/main/java/com/example/a10118389/api/api.java
+++
@@ -1,6 +0,0 @@
-package com.example.a10118389.api;
-
-public class api {
-    public static String Wisata = "https://dev.farizdotid.com/api/purwakarta/wisata";
-    public static String DetailWisata = "https://dev.farizdotid.com/api/purwakarta/wisata/{id}";
-}
