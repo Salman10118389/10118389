588c035c323c0771dd37226b67caf5e3133acd83
salman10118389 <salman.10118389@mahasiswa.unikom.ac.id>
2021-08-14 19:33:46 +0700

    Uas AKB 10118389 Muhammad Salman Al-Farisi

---
+++ app/src/main/java/com/example/a10118389/model/ModelMapLocation.java
@@ -0,0 +1,15 @@
+package com.example.a10118389.model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class ModelMapLocation {
+    public String name;
+    public LatLng center;
+
+    public ModelMapLocation(){}
+
+    public ModelMapLocation(String name, double lat, double lng){
+        this.name = name;
+        this .center = new LatLng(lat, lng);
+    }
+}
